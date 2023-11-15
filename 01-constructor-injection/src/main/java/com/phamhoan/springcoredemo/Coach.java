8d7497f868f4c0d6f6a36c7f09b017616cd14ead
Pham Van Hoan <phamvanhoan@luvina.net>
2023-11-15 10:20:10 +0700

    add project

---
+++ 01-constructor-injection/src/main/java/com/phamhoan/springcoredemo/Coach.java
@@ -0,0 +1,6 @@
+package com.phamhoan.springcoredemo;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 01-constructor-injection/src/main/java/com/phamhoan/springcoredemo/DemoController.java
@@ -0,0 +1,23 @@
+package com.phamhoan.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/daily-workout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
